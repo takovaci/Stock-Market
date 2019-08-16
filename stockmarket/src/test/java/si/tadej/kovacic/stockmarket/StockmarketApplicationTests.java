cf5a3096f9edf42fd6b17b50481e77848d9dd664
Tadej <tadej.kovacic@gmail.com>
2019-08-16 22:01:41 +0100

    initial commit

---
+++ stockmarket/src/test/java/si/tadej/kovacic/stockmarket/StockmarketApplicationTests.java
@@ -0,0 +1,16 @@
+package si.tadej.kovacic.stockmarket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockmarketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
